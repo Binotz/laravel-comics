6fb30fa327fd189eb8aeb0ab15be36270088c019
Binotz <binotto.riccardo@gmail.com>
2022-08-24 16:05:55 +0200

    Header prototype done

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="header-container">
+        <div class="header-top">
+            <span>DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+        <div class="header-bottom">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo">
+            <nav>
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">moveis</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </nav>
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+
+</header>

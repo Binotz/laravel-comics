b23531be9a3545ce6c843c27769d04f64ea503ff
Binotz <binotto.riccardo@gmail.com>
2022-08-25 13:09:17 +0200

    Footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,9 +2,122 @@
     <div class="footer-container">
         <div class="footer-top">
             <div class="footer-menu">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde expedita inventore quibusdam possimus. Tempora mollitia odio pariatur soluta tempore? Totam magnam nemo maxime amet nisi nulla quibusdam error sit atque.
+                <div class="footer-menu-left-col">
+                    <h2>dc commics</h2>
+                    <ul>
+                        <li>
+                            <a href="#">characters</a>
+                        </li>
+                        <li>
+                            <a href="#">comics</a>
+                        </li>
+                        <li>
+                            <a href="#">movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">games</a>
+                        </li>
+                        <li>
+                            <a href="#">videos</a>
+                        </li>
+                        <li>
+                            <a href="#">news</a>
+                        </li>
+                    </ul>
+
+                    <h2>shop</h2>
+                    <ul>
+                        <li>
+                            <a href="#">shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">shop DC Collectables</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+
+                <div class="footer-menu-center-col">
+                    <h2>dc</h2>
+                    <ul>
+                        <li>
+                            <a href="#">term Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy (New)</a>
+                        </li>
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">advertising</a>
+                        </li>
+                        <li>
+                            <a href="#">jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">subscription</a>
+                        </li>
+                        <li>
+                            <a href="#">talent workshop</a>
+                        </li>
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">shop help</a>
+                        </li>
+                        <li>
+                            <a href="#">contact us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer-menu-right-col">
+                    <h2>sites</h2>
+                    <ul>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+             <p>All site content tm and 2020 dc entertainment, unless <a href="#">noted here</a>. All rights reserved.
+            <a href="#">Cookies settings</a></p>
             </div>
             <img src="{{asset('images/dc-logo-bg.png')}}" alt="dc logo footer">
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="footer-bottom-container">
+            <div class="sign-up-now">
+                <span>sign-up now!</span>
+            </div>
+            <div class="icons">
+                <span>follow us</span>
+                <img src="{{asset('images/footer-facebook.png')}}" alt="Facebook">
+                <img src="{{asset('images/footer-twitter.png')}}" alt="Twitter">
+                <img src="{{asset('images/footer-youtube.png')}}" alt="YouTube">
+                <img src="{{asset('images/footer-pinterest.png')}}" alt="Pinterest">
+                <img src="{{asset('images/footer-periscope.png')}}" alt="Periscope">
+            </div>
         </div>
     </div>
 </footer>
